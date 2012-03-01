06e756b10a5428c2e51921e27ac4511a8b20fc08
Radovan Semancik <radovan.semancik@evolveum.com>
2012-03-01 18:15:11 +0000

    Added ConfigurationException. Simplified provisioning exceptions. Improved error reporting.

---
+++ infra/util/src/main/java/com/evolveum/midpoint/util/exception/ConfigurationException.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2011 Evolveum
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1 or
+ * CDDLv1.0.txt file in the source code distribution.
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * Portions Copyrighted 2011 [name of copyright owner]
+ */
+package com.evolveum.midpoint.util.exception;
+
+/**
+ * Configuration exception indicates that something is mis-configured.
+ * 
+ * The system or its part is misconfigured and therefore the intended operation
+ * cannot be executed.
+ * 
+ * @author Radovan Semancik
+ *
+ */
+public class ConfigurationException extends CommonException {
+
+	public ConfigurationException() {
+	}
+
+	public ConfigurationException(String message) {
+		super(message);
+	}
+
+	public ConfigurationException(Throwable cause) {
+		super(cause);
+	}
+
+	public ConfigurationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	@Override
+	public String getOperationResultMessage() {
+		return "Configuration error";
+	}
+
+}
