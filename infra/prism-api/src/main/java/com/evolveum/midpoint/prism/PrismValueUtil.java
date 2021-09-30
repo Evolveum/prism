af0492ec299f72bad9f072bf8a9373d4df1de254
Richard Richter <richard.richter@evolveum.com>
2021-09-30 20:35:38 +0200

    PrismValueUtil: added @Nullable getParentObject(PrismValue)
    Returns parent object, traversing multiple parent links to get there.

--- infra/prism-api/src/main/java/com/evolveum/midpoint/prism/PrismValueUtil.java
+++ infra/prism-api/src/main/java/com/evolveum/midpoint/prism/PrismValueUtil.java
@@ -1,32 +1,46 @@
 /*
- * Copyright (c) 2010-2018 Evolveum and contributors
+ * Copyright (C) 2010-2021 Evolveum and contributors
  *
  * This work is dual-licensed under the Apache License 2.0
  * and European Union Public License. See LICENSE file for details.
  */
-
 package com.evolveum.midpoint.prism;
-
-import com.evolveum.midpoint.prism.xnode.*;
-import com.evolveum.midpoint.util.exception.SchemaException;
-import org.apache.commons.lang.Validate;
-import org.jetbrains.annotations.NotNull;
 
 import javax.xml.namespace.QName;
 
-/**
- *
- */
+import org.apache.commons.lang.Validate;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import com.evolveum.midpoint.prism.xnode.ListXNode;
+import com.evolveum.midpoint.prism.xnode.RootXNode;
+import com.evolveum.midpoint.prism.xnode.XNode;
+import com.evolveum.midpoint.util.exception.SchemaException;
+
 public class PrismValueUtil {
 
+    @Nullable
     public static PrismContainerValue<?> getParentContainerValue(PrismValue value) {
         Itemable parent = value.getParent();
         if (parent instanceof Item) {
-            PrismValue parentParent = ((Item) parent).getParent();
-            return parentParent instanceof PrismContainerValue ? (PrismContainerValue) parentParent : null;
+            return ((Item<?, ?>) parent).getParent();
         } else {
             return null;
         }
+    }
+
+    /**
+     * Returns parent object, potentially traversing multiple parent links to get there.
+     */
+    @Nullable
+    public static PrismObject<?> getParentObject(@Nullable PrismValue value) {
+        while (value != null) {
+            if (value instanceof PrismObjectValue) {
+                return ((PrismObjectValue<?>) value).asPrismObject();
+            }
+            value = getParentContainerValue(value);
+        }
+        return null;
     }
 
     public static <T> PrismProperty<T> createRaw(@NotNull XNode node, @NotNull QName itemName, PrismContext prismContext)
@@ -48,8 +62,8 @@ public class PrismValueUtil {
     }
 
     public static boolean differentIds(PrismValue v1, PrismValue v2) {
-        Long id1 = v1 instanceof PrismContainerValue ? ((PrismContainerValue) v1).getId() : null;
-        Long id2 = v2 instanceof PrismContainerValue ? ((PrismContainerValue) v2).getId() : null;
+        Long id1 = v1 instanceof PrismContainerValue ? ((PrismContainerValue<?>) v1).getId() : null;
+        Long id2 = v2 instanceof PrismContainerValue ? ((PrismContainerValue<?>) v2).getId() : null;
         return id1 != null && id2 != null && id1.longValue() != id2.longValue();
     }
 }
