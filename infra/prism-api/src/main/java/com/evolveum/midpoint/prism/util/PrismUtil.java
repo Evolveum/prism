78908ae654b75ce4f2bc5268d1dfae092c029cea
Richard Richter <richard.richter@evolveum.com>
2021-10-26 19:35:09 +0200

    PrismUtil: added setPropertyNullAndComplete(obj, path) method

--- infra/prism-api/src/main/java/com/evolveum/midpoint/prism/util/PrismUtil.java
+++ infra/prism-api/src/main/java/com/evolveum/midpoint/prism/util/PrismUtil.java
@@ -1,14 +1,24 @@
 /*
- * Copyright (c) 2010-2019 Evolveum and contributors
+ * Copyright (C) 2010-2021 Evolveum and contributors
  *
  * This work is dual-licensed under the Apache License 2.0
  * and European Union Public License. See LICENSE file for details.
  */
 package com.evolveum.midpoint.prism.util;
 
+import static java.util.Collections.unmodifiableCollection;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import javax.xml.namespace.QName;
+
+import org.apache.commons.lang.StringUtils;
+
 import com.evolveum.midpoint.prism.*;
 import com.evolveum.midpoint.prism.delta.ItemDelta;
 import com.evolveum.midpoint.prism.match.MatchingRule;
+import com.evolveum.midpoint.prism.path.ItemPath;
 import com.evolveum.midpoint.prism.polystring.PolyString;
 import com.evolveum.midpoint.prism.xml.XsdTypeMapper;
 import com.evolveum.midpoint.util.DebugDumpable;
@@ -16,14 +26,6 @@ import com.evolveum.midpoint.util.DebugUtil;
 import com.evolveum.midpoint.util.QNameUtil;
 import com.evolveum.midpoint.util.exception.SchemaException;
 import com.evolveum.prism.xml.ns._public.types_3.PolyStringType;
-import org.apache.commons.lang.StringUtils;
-
-import javax.xml.namespace.QName;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.stream.Collectors;
-
-import static java.util.Collections.unmodifiableCollection;
 
 /**
  * TODO clean this up as it is part of prism-api!
@@ -38,7 +40,7 @@ public class PrismUtil {
         }
         // TODO: switch to Recomputable interface instead of PolyString
         if (realValue instanceof PolyString && prismContext != null) {
-            PolyString polyStringVal = (PolyString)realValue;
+            PolyString polyStringVal = (PolyString) realValue;
             // Always recompute. Recompute is cheap operation and this avoids a lot of bugs
             polyStringVal.recompute(prismContext.getDefaultPolyStringNormalizer());
         }
@@ -55,11 +57,7 @@ public class PrismUtil {
         return value == null || StringUtils.isEmpty(value.getOrig()) && StringUtils.isEmpty(value.getNorm());
     }
 
-//    public static PrismUnmarshaller getXnodeProcessor(@NotNull PrismContext prismContext) {
-//        return ((PrismContextImpl) prismContext).getPrismUnmarshaller();
-//    }
-
-    public static <T,X> PrismPropertyValue<X> convertPropertyValue(PrismPropertyValue<T> srcVal,
+    public static <T, X> PrismPropertyValue<X> convertPropertyValue(PrismPropertyValue<T> srcVal,
             PrismPropertyDefinition<T> srcDef, PrismPropertyDefinition<X> targetDef,
             PrismContext prismContext) {
         if (targetDef.getTypeName().equals(srcDef.getTypeName())) {
@@ -71,14 +69,14 @@ public class PrismUtil {
         }
     }
 
-    public static <T,X> PrismProperty<X> convertProperty(PrismProperty<T> srcProp, PrismPropertyDefinition<X> targetDef,
+    public static <T, X> PrismProperty<X> convertProperty(PrismProperty<T> srcProp, PrismPropertyDefinition<X> targetDef,
             PrismContext prismContext) throws SchemaException {
         if (targetDef.getTypeName().equals(srcProp.getDefinition().getTypeName())) {
             return (PrismProperty<X>) srcProp;
         } else {
             PrismProperty<X> targetProp = targetDef.instantiate();
             Class<X> expectedJavaType = XsdTypeMapper.toJavaType(targetDef.getTypeName());
-            for (PrismPropertyValue<T> srcPVal: srcProp.getValues()) {
+            for (PrismPropertyValue<T> srcPVal : srcProp.getValues()) {
                 X convertedRealValue = JavaTypeConverter.convert(expectedJavaType, srcPVal.getValue());
                 targetProp.add(prismContext.itemFactory().createPropertyValue(convertedRealValue));
             }
@@ -86,7 +84,7 @@ public class PrismUtil {
         }
     }
 
-    public static <O extends Objectable> void setDeltaOldValue(PrismObject<O> oldObject, ItemDelta<?,?> itemDelta) {
+    public static <O extends Objectable> void setDeltaOldValue(PrismObject<O> oldObject, ItemDelta<?, ?> itemDelta) {
         if (oldObject == null) {
             return;
         }
@@ -97,7 +95,7 @@ public class PrismUtil {
     }
 
     public static <O extends Objectable> void setDeltaOldValue(PrismObject<O> oldObject, Collection<? extends ItemDelta> itemDeltas) {
-        for(ItemDelta itemDelta: itemDeltas) {
+        for (ItemDelta itemDelta : itemDeltas) {
             setDeltaOldValue(oldObject, itemDelta);
         }
     }
@@ -112,7 +110,7 @@ public class PrismUtil {
         if (matchingRule == null) {
             if (a instanceof byte[]) {
                 if (b instanceof byte[]) {
-                    return Arrays.equals((byte[])a, (byte[])b);
+                    return Arrays.equals((byte[]) a, (byte[]) b);
                 } else {
                     return false;
                 }
@@ -161,7 +159,7 @@ public class PrismUtil {
 
     public static void debugDumpWithLabel(StringBuilder sb, String label, Containerable cc, int indent) {
         if (cc == null) {
-            DebugUtil.debugDumpWithLabel(sb, label, (DebugDumpable)null, indent);
+            DebugUtil.debugDumpWithLabel(sb, label, (DebugDumpable) null, indent);
         } else {
             DebugUtil.debugDumpWithLabel(sb, label, cc.asPrismContainerValue(), indent);
         }
@@ -180,5 +178,15 @@ public class PrismUtil {
     public static <T extends Freezable> Collection<T> freezeCollectionDeeply(Collection<T> freezables) {
         freezables.forEach(Freezable::freeze);
         return unmodifiableCollection(freezables);
+    }
+
+    /** Sets the property with the provided path on the provided prism object to null and complete, if found. */
+    public static <T extends Objectable> void setPropertyNullAndComplete(
+            PrismObject<T> prismObject, ItemPath path) {
+        PrismProperty<?> property = prismObject.findProperty(path);
+        if (property != null) {
+            property.setRealValue(null);
+            property.setIncomplete(false);
+        }
     }
 }
