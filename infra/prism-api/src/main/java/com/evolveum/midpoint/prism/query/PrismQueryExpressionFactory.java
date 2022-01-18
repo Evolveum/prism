06d9fe29d523f9b5fa740b6480aab7c49462d9e9
Tony Tkacik <tonydamage@gmail.com>
2022-01-18 08:34:22 +0100

    Added missing file
    Signed-off-by: Tony Tkacik <tonydamage@gmail.com>

---
+++ infra/prism-api/src/main/java/com/evolveum/midpoint/prism/query/PrismQueryExpressionFactory.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2021-2022 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+package com.evolveum.midpoint.prism.query;
+
+import java.util.Map;
+
+import com.evolveum.midpoint.prism.ExpressionWrapper;
+
+public interface PrismQueryExpressionFactory {
+
+
+    ExpressionWrapper parseScript(Map<String, String> namespaceContext, String language, String script);
+
+}
