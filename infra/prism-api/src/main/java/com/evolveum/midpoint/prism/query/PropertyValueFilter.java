ecc89cfe3ec932afefe273959e26c097e60d12e6
Tony Tkacik <tonydamage@gmail.com>
2022-05-04 16:27:00 +0200

    MID-6935: Introduced getMatchingRuleOrDefault to filters
    getMatchingRule: returns matching rule as it was defined in filter
    getMatchingRuleOrDefault: returns matching rule
      - from explicitly mentioned in filter when non-null
      - from property definition (xsd annotation a:matchingRule) if non-null

--- infra/prism-api/src/main/java/com/evolveum/midpoint/prism/query/PropertyValueFilter.java
+++ infra/prism-api/src/main/java/com/evolveum/midpoint/prism/query/PropertyValueFilter.java
@@ -7,6 +7,10 @@
 
 package com.evolveum.midpoint.prism.query;
 
+import javax.xml.namespace.QName;
+
+import org.jetbrains.annotations.Nullable;
+
 import com.evolveum.midpoint.prism.PrismPropertyDefinition;
 import com.evolveum.midpoint.prism.PrismPropertyValue;
 
@@ -16,5 +20,18 @@ import com.evolveum.midpoint.prism.PrismPropertyValue;
 public interface PropertyValueFilter<T> extends ValueFilter<PrismPropertyValue<T>, PrismPropertyDefinition<T>> {
 
     // TODO cleanup this mess - how values are cloned, that expression is not cloned in LT/GT filter etc
+    @Override
     PropertyValueFilter clone();
+
+    default @Nullable QName getMatchingRuleOrDefault() {
+        QName maybe = getMatchingRule();
+        if (maybe != null) {
+            return maybe;
+        }
+        var definition = getDefinition();
+        if (definition != null) {
+            return definition.getMatchingRuleQName();
+        }
+        return null;
+    }
 }
