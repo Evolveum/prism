5665339a983ab41f07da4bad5f43a6cfc828ccd9
Richard Richter <richard.richter@evolveum.com>
2022-08-17 12:35:19 +0200

    TestCodeGeneration: TODO added, this is not in suite, should it be?

--- infra/prism-codegen/src/test/java/com/evolveum/prism/codegen/impl/TestCodeGeneration.java
+++ infra/prism-codegen/src/test/java/com/evolveum/prism/codegen/impl/TestCodeGeneration.java
@@ -9,21 +9,22 @@ package com.evolveum.prism.codegen.impl;
 import java.io.File;
 import java.io.IOException;
 
+import com.sun.codemodel.writer.FileCodeWriter;
 import org.testng.annotations.Test;
 
 import com.evolveum.midpoint.prism.AbstractPrismTest;
 import com.evolveum.midpoint.prism.ComplexTypeDefinition;
 import com.evolveum.midpoint.prism.PrismContext;
 import com.evolveum.midpoint.prism.PrismInternalTestUtil;
+import com.evolveum.midpoint.tools.testng.UnusedTestElement;
 import com.evolveum.prism.codegen.binding.BindingContext;
 import com.evolveum.prism.codegen.binding.TypeBinding;
-import com.sun.codemodel.writer.FileCodeWriter;
 
+@UnusedTestElement // TODO do we want this in testng-unit.xml?
 public class TestCodeGeneration extends AbstractPrismTest {
 
     private static final File TARGET = new File("target/test-code");
     private static final String TEST_JAVA_NS = "com.evolveum.prism.example.foo";
-
 
     @Test
     public void test000Mapping() throws IOException, CodeGenerationException {
@@ -35,13 +36,12 @@ public class TestCodeGeneration extends AbstractPrismTest {
         generator.addSchemas(prismContext.getSchemaRegistry().getSchemas());
         generator.addNamespaceMapping(PrismInternalTestUtil.NS_FOO, TEST_JAVA_NS);
 
-
         FileCodeWriter writer = new FileCodeWriter(TARGET);
-        BindingContext context =  generator.process();
+        BindingContext context = generator.process();
 
         var codeGen = new CodeGenerator(writer, generator);
 
-        TypeBinding binding =  generator.requireBinding(userType.getTypeName());
+        TypeBinding binding = generator.requireBinding(userType.getTypeName());
 
         codeGen.process(generator.requireBinding(PrismInternalTestUtil.OBJECT_TYPE_QNAME));
         codeGen.process(binding);
