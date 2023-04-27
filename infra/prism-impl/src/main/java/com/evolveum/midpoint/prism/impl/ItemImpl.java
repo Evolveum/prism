Evolveum/prism

commit a73dcc4facb8b96dac664d47fe539813de9b6f29

MID-7008: Added early exit to diff* methods in case they are used via equals
In order to determine if objects are equals, we do not need full diff of objects
only to know that they are different.
Adds optional exit of diff methods on first occurence of different value.

infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/ItemImpl.java
@@ -586,8 +586,18 @@ public abstract class ItemImpl<V extends PrismValue, D extends ItemDefinition> e
 
     void diffInternal(Item<V, D> other, Collection<? extends ItemDelta> deltas, boolean rootValuesOnly,
             ParameterizedEquivalenceStrategy strategy) {
+        diffInternal(other, deltas, rootValuesOnly, strategy, false);
+    }
+
+    boolean diffInternal(Item<V, D> other, Collection<? extends ItemDelta> deltas, boolean rootValuesOnly,
+            ParameterizedEquivalenceStrategy strategy, boolean exitOnDiff) {
         ItemDelta delta = createDelta();
         if (other == null) {
+            // Early exit for equals use case
+            if (exitOnDiff) {
+                return true;
+            }
+
             if (delta.getDefinition() == null && this.getDefinition() != null) {
                 delta.setDefinition(this.getDefinition().clone());
             }
@@ -611,7 +621,11 @@ public abstract class ItemImpl<V extends PrismValue, D extends ItemDefinition> e
                     if (!rootValuesOnly && thisValue.representsSameValue(otherValue, true)) {
                         found = true;
                         // Matching IDs, look inside to figure out internal deltas
-                        ((PrismValueImpl) thisValue).diffMatchingRepresentation(otherValue, deltas, strategy);
+                        boolean different = ((PrismValueImpl) thisValue).diffMatchingRepresentation(otherValue, deltas, strategy, exitOnDiff);
+                        if (exitOnDiff && different) {
+                            return true;
+                        }
+
                         // No need to process this value again
                         iterator.remove();
                         break;
@@ -624,12 +638,18 @@ public abstract class ItemImpl<V extends PrismValue, D extends ItemDefinition> e
                     }
                 }
                 if (!found) {
+                    if (exitOnDiff) {
+                        return true;
+                    }
                     // We have the value and the other does not, this is delete of the entire value
                     delta.addValueToDelete(thisValue.clone());
                 }
             }
             // outstandingOtherValues are those values that the other has and we could not
             // match them to any of our values. These must be new values to add
+            if (exitOnDiff && !outstandingOtherValues.isEmpty()) {
+                return true;
+            }
             for (PrismValue outstandingOtherValue : outstandingOtherValues) {
                 delta.addValueToAdd(outstandingOtherValue.clone());
             }
@@ -640,6 +660,7 @@ public abstract class ItemImpl<V extends PrismValue, D extends ItemDefinition> e
         if (delta != null && !delta.isEmpty()) {
             ((Collection)deltas).add(delta);
         }
+        return !delta.isEmpty();
     }
 
     protected ItemDelta<V,D> fixupDelta(ItemDelta<V, D> delta, Item<V, D> other) {

infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/PrismValueImpl.java
@@ -70,14 +70,17 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
         this.parent = parent;
     }
 
+    @Override
     public void setPrismContext(PrismContext prismContext) {
         this.prismContext = prismContext;
     }
 
+    @Override
     public void setOriginObject(Objectable source) {
         this.originObject = source;
     }
 
+    @Override
     public void setOriginType(OriginType type) {
         this.originType = type;
     }
@@ -92,6 +95,7 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
         return originObject;
     }
 
+    @Override
     public Map<String, Object> getUserData() {
         return userData;
     }
@@ -175,6 +179,7 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
         // Do nothing by default
     }
 
+    @Override
     public void revive(PrismContext prismContext) throws SchemaException {
         if (this.prismContext == null) {
             this.prismContext = prismContext;
@@ -193,6 +198,7 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
         recompute(getPrismContext());
     }
 
+    @Override
     public abstract void recompute(PrismContext prismContext);
 
     @Override
@@ -211,8 +217,10 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
         }
     }
 
+    @Override
     public abstract void checkConsistenceInternal(Itemable rootItem, boolean requireDefinitions, boolean prohibitRaw, ConsistencyCheckScope scope);
 
+    @Override
     public boolean representsSameValue(PrismValue other, boolean lax) {
         return false;
     }
@@ -225,6 +233,7 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
     /**
      * Literal clone.
      */
+    @Override
     public abstract PrismValue clone();
 
     @Override
@@ -238,6 +247,7 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
      * Complex clone with different cloning strategies.
      * @see CloneStrategy
      */
+    @Override
     public abstract PrismValue cloneComplex(CloneStrategy strategy);
 
     protected void copyValues(CloneStrategy strategy, PrismValueImpl clone) {
@@ -282,6 +292,7 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
         }
     }
 
+    @Override
     @SuppressWarnings("SimplifiableIfStatement")
     public boolean equals(PrismValue other, @NotNull ParameterizedEquivalenceStrategy strategy) {
         // parent is not considered at all. it is not relevant.
@@ -295,6 +306,7 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
     }
 
     // original equals was "isLiteral = false"!
+    @Override
     public boolean equals(Object other) {
         return this == other ||
                 (other == null || other instanceof PrismValue) &&
@@ -312,9 +324,15 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
         return itemDeltas;
     }
 
-    public void diffMatchingRepresentation(PrismValue otherValue,
+    public final void diffMatchingRepresentation(PrismValue otherValue,
             Collection<? extends ItemDelta> deltas, ParameterizedEquivalenceStrategy strategy) {
+        diffMatchingRepresentation(otherValue, deltas, strategy, false);
+    }
+
+    public boolean diffMatchingRepresentation(PrismValue otherValue,
+            Collection<? extends ItemDelta> deltas, ParameterizedEquivalenceStrategy strategy, boolean exitOnDiff) {
         // Nothing to do by default
+        return false;
     }
 
     protected void appendOriginDump(StringBuilder builder) {
@@ -328,11 +346,14 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
         }
     }
 
+    @Override
     public abstract String toHumanReadableString();
 
+    @Override
     @Nullable
     abstract public Class<?> getRealClass();
 
+    @Override
     @Nullable
     abstract public <T> T getRealValue();
 
@@ -341,6 +362,7 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
     // of AccessCertificationCampaignType.
     //
     // Generally, this method returns either "this" (PrismValue) or a PrismContainerValue.
+    @Override
     public PrismValue getRootValue() {
         PrismValue current = this;
         for (;;) {
@@ -352,10 +374,12 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
         }
     }
 
+    @Override
     public PrismContainerValue<?> getParentContainerValue() {
         return PrismValueUtil.getParentContainerValue(this);
     }
 
+    @Override
     public QName getTypeName() {
         ItemDefinition definition = getDefinition();
         return definition != null ? definition.getTypeName() : null;
@@ -363,6 +387,7 @@ public abstract class PrismValueImpl extends AbstractFreezable implements PrismV
 
     // Path may contain ambiguous segments (e.g. assignment/targetRef when there are more assignments)
     // Note that the path can contain name segments only (at least for now)
+    @Override
     @NotNull
     public Collection<PrismValue> getAllValues(ItemPath path) {
         if (path.isEmpty()) {

commit 1d5f89113839df5eb732ad7b1def97e285065889

Stop creating deltas during prism equality checks
This improves the "fast fail" diff for equality checks originally
introduced in a73dcc4facb8b96dac664d47fe539813de9b6f29: we no longer
create deltas when comparing prism values.

infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/ItemImpl.java
@@ -575,28 +575,26 @@ public abstract class ItemImpl<V extends PrismValue, D extends ItemDefinition<?>
 
     boolean diffInternal(Item<V, D> other, Collection<? extends ItemDelta> deltas, boolean rootValuesOnly,
             ParameterizedEquivalenceStrategy strategy, boolean exitOnDiff) {
-        ItemDelta delta = createDelta();
+        ItemDelta delta = exitOnDiff ? null : createDelta();
         if (other == null) {
             // Early exit for equals use case
-            if (exitOnDiff && hasAnyValue()) {
-                return true;
+            if (exitOnDiff) {
+                return hasAnyValue();
             }
-
-            if (delta.getDefinition() == null && this.getDefinition() != null) {
-                delta.setDefinition(this.getDefinition().clone());
-            }
-            //other doesn't exist, so delta means delete all values
+            // other doesn't exist, so delta means delete all values
             for (PrismValue value : getValues()) {
                 PrismValue valueClone = value.clone();
                 delta.addValueToDelete(valueClone);
             }
         } else {
-            if (delta.getDefinition() == null && other.getDefinition() != null) {
-                delta.setDefinition(other.getDefinition().clone());
+            if (delta != null && delta.getDefinition() == null) {
+                D otherDefinition = other.getDefinition();
+                if (otherDefinition != null) {
+                    delta.setDefinition(otherDefinition.clone()); // TODO why cloning?
+                }
             }
             // the other exists, this means that we need to compare the values one by one
-            Collection<PrismValue> outstandingOtherValues = new ArrayList<>(other.getValues().size());
-            outstandingOtherValues.addAll(other.getValues());
+            Collection<PrismValue> outstandingOtherValues = new ArrayList<>(other.getValues());
             for (PrismValue thisValue : getValues()) {
                 Iterator<PrismValue> iterator = outstandingOtherValues.iterator();
                 boolean found = false;
@@ -605,43 +603,45 @@ public abstract class ItemImpl<V extends PrismValue, D extends ItemDefinition<?>
                     if (!rootValuesOnly && thisValue.representsSameValue(otherValue, true)) {
                         found = true;
                         // Matching IDs, look inside to figure out internal deltas
-                        boolean different = ((PrismValueImpl) thisValue).diffMatchingRepresentation(otherValue, deltas, strategy, exitOnDiff);
+                        boolean different =
+                                ((PrismValueImpl) thisValue).diffMatchingRepresentation(otherValue, deltas, strategy, exitOnDiff);
                         if (exitOnDiff && different) {
                             return true;
                         }
-
-                        // No need to process this value again
-                        iterator.remove();
+                        iterator.remove(); // No need to process this value again
                         break;
                     } else if (thisValue.equals(otherValue, strategy)) {
                         found = true;
-                        // same values. No delta
-                        // No need to process this value again
-                        iterator.remove();
+                        iterator.remove(); // Same values. No delta. No need to process this value again.
                         break;
                     }
                 }
                 if (!found) {
                     if (exitOnDiff) {
                         return true;
+                    } else {
+                        assert delta != null;
+                        // We have the value and the other does not, this is delete of the entire value
+                        delta.addValueToDelete(thisValue.clone());
                     }
-                    // We have the value and the other does not, this is delete of the entire value
-                    delta.addValueToDelete(thisValue.clone());
                 }
             }
             // outstandingOtherValues are those values that the other has and we could not
             // match them to any of our values. These must be new values to add
-            if (exitOnDiff && !outstandingOtherValues.isEmpty()) {
-                return true;
+            if (exitOnDiff) {
+                if (!outstandingOtherValues.isEmpty()) {
+                    return true;
+                }
+            } else {
+                assert delta != null;
+                for (PrismValue outstandingOtherValue : outstandingOtherValues) {
+                    delta.addValueToAdd(outstandingOtherValue.clone());
+                }
+                // Some deltas may need to be polished a bit. E.g. transforming add/delete delta to a replace delta.
+                delta = fixupDelta(delta, other);
             }
-            for (PrismValue outstandingOtherValue : outstandingOtherValues) {
-                delta.addValueToAdd(outstandingOtherValue.clone());
-            }
-            // Some deltas may need to be polished a bit. E.g. transforming
-            // add/delete delta to a replace delta.
-            delta = fixupDelta(delta, other);
         }
-        if (ItemDelta.isEmpty(delta)) {
+        if (exitOnDiff || ItemDelta.isEmpty(delta)) {
             return false;
         } else {
             ((Collection) deltas).add(delta);