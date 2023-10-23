8996e031c4ec2e33f0d9fbb23a2f8cdb10b86318
dominikhorvath <dominikhorvath138@gmail.com>
2023-10-23 21:07:06 +0200

    update Axiom QL Visitor

--- infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/query/lang/AxiomQueryCompletionsVisitor.java
+++ infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/query/lang/AxiomQueryCompletionsVisitor.java
@@ -1,9 +1,17 @@
 package com.evolveum.midpoint.prism.impl.query.lang;
 
+import com.evolveum.axiom.lang.antlr.query.AxiomQueryParser;
 import com.evolveum.axiom.lang.antlr.query.AxiomQueryParserBaseVisitor;
+
+import com.evolveum.midpoint.prism.PrismContext;
 
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.RuleNode;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+import static com.evolveum.midpoint.prism.impl.query.lang.FilterNames.*;
 
 /**
  * Created by Dominik.
@@ -15,20 +23,66 @@ public class AxiomQueryCompletionsVisitor extends AxiomQueryParserBaseVisitor<Ob
     @Override
     public Object visitChildren(RuleNode node) {
         Object result = this.defaultResult();
-        int index = node.getChildCount();
-        ParseTree child = node.getChild(index -1);
-        // skip SEP token
-        if (child.getText().equals(" ")) {
-            child = node.getChild(index -2);
+        int childCount = node.getChildCount();
+
+        for(int i = 0; i < childCount && this.shouldVisitNextChild(node, result); ++i) {
+            ParseTree c = node.getChild(i);
+            // skip SEP token
+            if (!c.getText().equals(" ")) {
+                Object childResult = c.accept(this);
+                result = this.aggregateResult(result, childResult);
+            }
         }
 
-        Object childResult = child.accept(this);
-        result = this.aggregateResult(result, childResult);
-        // return last node
-        if (result == null) {
-            return node;
+        if (node.getRuleContext().getRuleIndex() == AxiomQueryParser.RULE_itemFilter) {
+            try {
+                validateItemFilter(node);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         return result;
+    }
+
+    private void validateItemFilter(@NotNull ParseTree itemFilter) throws ClassNotFoundException {
+        int childCount = itemFilter.getChildCount();
+        PrismContext prismContext = PrismContext.get();
+
+
+        for(int i = 0; i < childCount; ++i) {
+            ParseTree itemFilterChild = itemFilter.getChild(i);
+            if (!itemFilterChild.getText().equals(" ")) {
+                RuleNode nodeItemFilter = (RuleNode) itemFilterChild;
+                // find Type class by type or @type filterNames
+                if (
+                    (nodeItemFilter.getRuleContext().getRuleIndex() == AxiomQueryParser.RULE_path && nodeItemFilter.getText().equals(META_TYPE)) ||
+                    (nodeItemFilter.getRuleContext().getRuleIndex() == AxiomQueryParser.RULE_filterName && nodeItemFilter.getText().equals(TYPE.getLocalPart()))
+                ) {
+                    Class typeClass = Class.forName(Objects.requireNonNull(getLastNode(nodeItemFilter)).getText());
+                    prismContext.getSchemaRegistry().findObjectDefinitionByCompileTimeClass(typeClass);
+                }
+                // find ItemPath
+                if (nodeItemFilter.getRuleContext().getRuleIndex() == AxiomQueryParser.RULE_path) {
+
+                }
+            }
+        }
+    }
+
+    private ParseTree getLastNode(RuleNode currentPosition) {
+        while (currentPosition.getRuleContext().getRuleIndex() != AxiomQueryParser.RULE_itemFilter) {
+            currentPosition = (RuleNode) currentPosition.getParent();
+            return currentPosition.getChild(currentPosition.getChildCount() - 1);
+        }
+        return null;
+    }
+
+    private ParseTree getFirstNode(RuleNode currentPosition) {
+        while (currentPosition.getRuleContext().getRuleIndex() != AxiomQueryParser.RULE_itemFilter) {
+            currentPosition = (RuleNode) currentPosition.getParent();
+            return currentPosition.getChild(0);
+        }
+        return null;
     }
 }
