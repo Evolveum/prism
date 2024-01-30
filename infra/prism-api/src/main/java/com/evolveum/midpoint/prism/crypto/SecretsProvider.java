f90f711ad75bf865dc3f113d1ab24e073ace24db
Viliam Repan <vilo.repan@evolveum.com>
2024-01-30 14:42:25 +0100

    secret providers, some work on configuration

---
+++ infra/prism-api/src/main/java/com/evolveum/midpoint/prism/crypto/SecretsProvider.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2010-2024 Evolveum and contributors
+ *
+ * This work is dual-licensed under the Apache License 2.0
+ * and European Union Public License. See LICENSE file for details.
+ */
+
+package com.evolveum.midpoint.prism.crypto;
+
+import java.nio.ByteBuffer;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface SecretsProvider {
+
+    @NotNull String getIdentifier();
+
+    String getSecretString(@NotNull String key) throws EncryptionException;
+
+    default ByteBuffer getSecretBinary(@NotNull String key) throws EncryptionException {
+        String secretString = getSecretString(key);
+        if (secretString == null) {
+            return null;
+        }
+        return ByteBuffer.wrap(secretString.getBytes());
+    }
+}
