48e765dc24a36cdf1b732c63f78880bc198925e6
Tony Tkáčik <tonydamage@gmail.com>
2024-03-20 10:27:22 +0100

    MID-9506: Added schema notion to Axiom Query Visitors

--- infra/prism-api/src/main/java/com/evolveum/midpoint/prism/query/AxiomQueryLangService.java
+++ infra/prism-api/src/main/java/com/evolveum/midpoint/prism/query/AxiomQueryLangService.java
@@ -2,20 +2,31 @@ package com.evolveum.midpoint.prism.query;
 
 import com.evolveum.axiom.lang.antlr.AxiomQueryError;
 
-import com.evolveum.midpoint.prism.Containerable;
 import com.evolveum.midpoint.prism.ItemDefinition;
 
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.xml.namespace.QName;
 import java.util.List;
 import java.util.Map;
 
 public interface AxiomQueryLangService {
 
-    List<AxiomQueryError> validate(@Nullable ItemDefinition definition,  String query);
+    @Deprecated
+    default List<AxiomQueryError> validate(String query) {
+        return validate(null, query);
+    }
 
-    Map<String, String> queryCompletion(@Nullable ItemDefinition definition, String query);
+
+    @Deprecated
+    default Map<String, String> queryCompletion(String query) {
+        return queryCompletion(null, query);
+    }
+
+    List<AxiomQueryError> validate(@Nullable ItemDefinition<?> rootItem, String query);
+
+    // FIXME: Return value should be more structured, e.g item definition, filter definition (with help text?)
+    //        type definition?
+    //        Something like suggested text, documentation for suggestion?
+    Map<String, String> queryCompletion(@Nullable ItemDefinition<?> rootItem, String query);
 
 }
