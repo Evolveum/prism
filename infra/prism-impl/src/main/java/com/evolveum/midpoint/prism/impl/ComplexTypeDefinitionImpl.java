ab727f7a5250a7e91dd42b6b799bb15234b84c5a
Tony Tkáčik <tonydamage@gmail.com>
2024-04-23 12:48:58 +0200

    Fixed infra items not found in some cases
    Signed-off-by: Tony Tkáčik <tonydamage@gmail.com>

--- infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/ComplexTypeDefinitionImpl.java
+++ infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/ComplexTypeDefinitionImpl.java
@@ -368,8 +368,14 @@ public class ComplexTypeDefinitionImpl
         return null;
     }
 
+
+
     @Override
     public <ID extends ItemDefinition<?>> ID findLocalItemDefinition(@NotNull QName name, @NotNull Class<ID> clazz, boolean caseInsensitive) {
+        if (name instanceof InfraItemName infra) {
+            return findInfraItemDefinition(infra, infra.rest(), clazz);
+        }
+
         var explicit = ComplexTypeDefinition.super.findLocalItemDefinition(name, clazz, caseInsensitive);
         if (explicit != null) {
             return explicit;
