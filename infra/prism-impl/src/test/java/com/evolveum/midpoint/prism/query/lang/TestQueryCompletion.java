8ef6e3843ef70e25d77509c108930f8361e0c2ad
dominikhorvath <dominikhorvath138@gmail.com>
2024-06-12 23:23:38 +0200

    update TestQueryCompletion

--- infra/prism-impl/src/test/java/com/evolveum/midpoint/prism/query/lang/TestQueryCompletion.java
+++ infra/prism-impl/src/test/java/com/evolveum/midpoint/prism/query/lang/TestQueryCompletion.java
@@ -1,10 +1,10 @@
 package com.evolveum.midpoint.prism.query.lang;
 
-import static com.evolveum.midpoint.prism.PrismInternalTestUtil.DEFAULT_NAMESPACE_PREFIX;
-
 import java.io.IOException;
 import java.util.*;
 import javax.xml.namespace.QName;
+
+import com.evolveum.midpoint.prism.foo.UserType;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
@@ -12,11 +12,9 @@ import org.testng.annotations.Test;
 import org.xml.sax.SAXException;
 
 import com.evolveum.midpoint.prism.*;
-import com.evolveum.midpoint.prism.foo.UserType;
 import com.evolveum.midpoint.prism.impl.query.lang.AxiomQueryLangServiceImpl;
 import com.evolveum.midpoint.prism.schema.SchemaRegistry;
 import com.evolveum.midpoint.prism.util.PrismTestUtil;
-import com.evolveum.midpoint.util.PrettyPrinter;
 import com.evolveum.midpoint.util.exception.SchemaException;
 
 /**
@@ -25,22 +23,21 @@ import com.evolveum.midpoint.util.exception.SchemaException;
 public class TestQueryCompletion extends AbstractPrismTest {
 
     private AxiomQueryLangServiceImpl axiomQueryLangServiceImpl;
-
     private SchemaRegistry schemaRegistry;
+    private PrismObjectDefinition<UserType> userDef;
 
     @BeforeSuite
     public void setupDebug() throws SchemaException, SAXException, IOException {
-        PrettyPrinter.addDefaultNamespacePrefix(DEFAULT_NAMESPACE_PREFIX);
         PrismTestUtil.resetPrismContext(new PrismInternalTestUtil());
         axiomQueryLangServiceImpl = new AxiomQueryLangServiceImpl(PrismContext.get());
-        schemaRegistry = PrismContext.get().getSchemaRegistry();
+        schemaRegistry = getPrismContext().getSchemaRegistry();
+        userDef = schemaRegistry.findObjectDefinitionByType(UserType.COMPLEX_TYPE);
     }
 
-    // Basic filters
     @Test
     public void testQueryCompletionDot() {
         String query = ". ";
-        Map<String, String> suggestions = axiomQueryLangServiceImpl.queryCompletion(null, query);
+        Map<String, String> suggestions = axiomQueryLangServiceImpl.queryCompletion(userDef, query);
         Map<String, String> expected = new HashMap<>();
         expected.put("isRoot", null);
         expected.put("inOrg", null);
@@ -55,32 +52,31 @@ public class TestQueryCompletion extends AbstractPrismTest {
     @Test
     public void testQueryCompletionTypesOfUserType() {
         String query = ". type ";
+        Map<String, String> suggestions = axiomQueryLangServiceImpl.queryCompletion(userDef, query);
         TypeDefinition typeDefinition = schemaRegistry.findTypeDefinitionByType(new QName("UserType"));
-        Map<String, String> suggestions = axiomQueryLangServiceImpl.queryCompletion(null, query);
         List<String> objectTypes = schemaRegistry.getAllSubTypesByTypeDefinition(Collections.singletonList(typeDefinition)).stream().map(item -> item.getTypeName().getLocalPart()).sorted().toList();
         Assert.assertEquals(suggestions.keySet().stream().sorted().toList(), objectTypes);
     }
 
-    @Test
+    @Test(enabled = false)
     public void testQueryCompletionBasePathsOfUserType() {
         String query = ". type UserType and ";
+        Map<String, String> suggestions = axiomQueryLangServiceImpl.queryCompletion(userDef, query);
         TypeDefinition typeDefinition = schemaRegistry.findTypeDefinitionByType(new QName("UserType"));
         PrismObjectDefinition<?> objectDefinition = schemaRegistry.findObjectDefinitionByCompileTimeClass((Class) typeDefinition.getCompileTimeClass());
         List<String> itemPaths = new ArrayList<>(objectDefinition.getItemNames().stream().map(QName::getLocalPart).toList());
         itemPaths.add(".");
-        Map<String, String> suggestions = axiomQueryLangServiceImpl.queryCompletion(null, query);
         Assert.assertEquals(suggestions.keySet().stream().sorted().toList(), itemPaths.stream().sorted().toList());
     }
 
-    @Test(enabled = false)
+    @Test()
     public void testQueryCompletionBaseFilterName() {
         String query = ". type UserType and givenName ";
+        Map<String, String> suggestions = axiomQueryLangServiceImpl.queryCompletion(userDef, query);
 
         List<String> expected = new ArrayList<String>();
         expected.add("levenshtein");
         expected.add("greaterOrEqual");
-        expected.add("isRoot");
-        expected.add("inOrg");
         expected.add("lessOrEqual");
         expected.add("notEqual");
         expected.add("fullText");
@@ -94,24 +90,20 @@ public class TestQueryCompletion extends AbstractPrismTest {
         expected.add("exists");
         expected.add("anyIn");
         expected.add("greater");
-        expected.add("ownedBy");
-        expected.add("inOid");
         expected.add("startsWith");
         expected.add("not");
 
-        Map<String, String> suggestions = axiomQueryLangServiceImpl.queryCompletion(null, query);
         Assert.assertEquals(suggestions.keySet().stream().sorted().toList(), expected.stream().sorted().toList());
     }
 
     @Test
     public void testQueryCompletionBaseSubFilter() {
         String query = ". type UserType and givenName equal \"Jack\" ";
-        Map<String, String> suggestions = axiomQueryLangServiceImpl.queryCompletion(null, query);
+        Map<String, String> suggestions = axiomQueryLangServiceImpl.queryCompletion(userDef, query);
         Assert.assertEquals(suggestions.keySet().stream().sorted().toList(), List.of("and", "or"));
     }
 
-    // Advanced filters
-    @Test(enabled = false)
+    @Test()
     public void testUserExtensionItemPath() {
         PrismObjectDefinition<UserType> userDef = schemaRegistry.findObjectDefinitionByType(UserType.COMPLEX_TYPE);
         String query = "extension/";
