2ea5c3fe9e761998dd382882aa10056a297ae83d
Tony Tkáčik <tonydamage@gmail.com>
2024-11-05 13:52:05 +0100

    Lazy Parsing: improved behaviour when container is parsed
      - Container is not parsed if:
        - isEmpty / hasItems checks are performed
        - is frozen (Xnodes are immutable and it is safe to parse later)
        - is cloned (Xnodes are "immutable" and it is safe to parse later)
        - getDefinition() is called
    
      - Deprecated code is set of testing methods, which triggers
        breakpoint when materialization is triggered with exclusion
        for valid use-cases, these are disabled by default and are
        intented only for manual testing
    
    (cherry picked from commit eb7aabbf5437e51275a08ee6892b60fb96d37883)

--- infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/lazy/LazyPrismContainerValue.java
+++ infra/prism-impl/src/main/java/com/evolveum/midpoint/prism/impl/lazy/LazyPrismContainerValue.java
@@ -12,6 +12,7 @@ import com.evolveum.midpoint.prism.impl.PrismContextImpl;
 import com.evolveum.midpoint.prism.impl.marshaller.PrismUnmarshaller;
 import com.evolveum.midpoint.prism.impl.xnode.MapXNodeImpl;
 
+import com.evolveum.midpoint.prism.lazy.LazyXNodeBasedPrismValue;
 import com.evolveum.midpoint.util.exception.SchemaException;
 
 import com.evolveum.midpoint.util.exception.SystemException;
@@ -21,8 +22,14 @@ import org.jetbrains.annotations.NotNull;
 import javax.xml.namespace.QName;
 
 public class LazyPrismContainerValue<C extends Containerable>
-        extends LazyXNodeBasedValue<MapXNodeImpl, PrismContainerValue<C>>
+        extends LazyXNodeBasedPrismValue<MapXNodeImpl, PrismContainerValue<C>>
         implements PrismContainerValueDelegator<C> {
+
+    /**
+     * Only for manual testing, will be removed later
+     */
+    @Deprecated
+    private static final boolean TRACK_CALLERS = false;
 
     protected ComplexTypeDefinition complexTypeDefinition = null;
 
@@ -30,6 +37,14 @@ public class LazyPrismContainerValue<C extends Containerable>
 
     public LazyPrismContainerValue(ComplexTypeDefinition ctd, ParsingContext parsingContext, @NotNull MapXNodeImpl xnode) {
         super(parsingContext, xnode);
+        this.complexTypeDefinition = ctd;
+    }
+
+    public LazyPrismContainerValue(LazyPrismContainerValue<C> other) {
+        super(other);
+        this.complexTypeDefinition = other.complexTypeDefinition;
+        this.parent = other.parent;
+
     }
 
     @Override
@@ -67,10 +82,17 @@ public class LazyPrismContainerValue<C extends Containerable>
 
     @Override
     protected PrismContainerValue<C> materialize(Source<MapXNodeImpl> source) {
+        trackCallers();
         try {
             var ret = ((PrismContextImpl) PrismContext.get()).getPrismUnmarshaller()
                     .parseRealContainerValueFromMap(source.value(), containerDef(), source.parsingContext());
+            // We set parent, if it was provided before materialization
             ret.setParent(parent);
+            if (isImmutable()) {
+                // If we were frozen before materialization, we freeze result.
+                ret.freeze();
+            }
+
             return ret;
         } catch (SchemaException e) {
             throw new SystemException("Error during lazy parse of PCV", e);
@@ -85,7 +107,15 @@ public class LazyPrismContainerValue<C extends Containerable>
         }
     }
 
+    @Override
+    public void accept(Visitor visitor) {
+        PrismContainerValueDelegator.super.accept(visitor);
+    }
 
+    @Override
+    public void acceptParentVisitor(Visitor visitor) {
+        PrismContainerValueDelegator.super.acceptParentVisitor(visitor);
+    }
 
     @Override
     public PrismContainerable<C> getParent() {
@@ -105,10 +135,109 @@ public class LazyPrismContainerValue<C extends Containerable>
         return "";
     }
 
+
+
     private PrismContainerDefinition<C> containerDef() {
         if (parent != null) {
             return (PrismContainerDefinition<C>) parent.getDefinition();
         }
+        // FIXME: maybe this should be illegalState? We should not have lazy without parent.
         return null;
+    }
+
+    @Override
+    public PrismContainerDefinition<C> getDefinition() {
+        return containerDef();
+    }
+
+    @Override
+    public PrismContainerValue<C> cloneComplex(CloneStrategy strategy) {
+        if (strategy == CloneStrategy.LITERAL && !isMaterialized()) {
+            var ret =  new LazyPrismContainerValue<>(this);
+            if (!ret.isMaterialized()) {
+                // During calling constructor, other thread may materialized value during copying of parent value
+                // so if copy is already materialized it may share delegate
+                // so it is safe only to return unmaterialized copy
+                return ret;
+            }
+        }
+        return PrismContainerValueDelegator.super.cloneComplex(strategy);
+    }
+
+    @Override
+    protected void performFreeze() {
+        if (isMaterialized()) {
+            // We freeze delegate if it is already materialized
+            delegate().freeze();
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        var raw = xnode();
+        if (raw != null) {
+            // If Value contains only XNode with container id, so it still considered nonEmpty (as per PrismContainerValueImpl.isEmpty)
+            return raw.isEmpty(); // isEmpty checks may not need triggering of materialization (maybe container id and incomplete?)
+
+        }
+        return PrismContainerValueDelegator.super.isEmpty();
+    }
+
+    @Override
+    public boolean hasNoItems() {
+        var raw = xnode();
+        if (raw != null) {
+            if (raw.isEmpty()) {
+                // Value is empty (no nested xnodes)
+                return true;
+            }
+            // If Value contains only XNode with container id, it has no items,
+            // otherwise it has items.
+            return raw.size() == 1 && raw.containsKey(PrismConstants.T_ID);
+        }
+        return PrismContainerValueDelegator.super.isEmpty();
+    }
+
+    /**
+     * Testing method, controlled by  #TRACK_CALLERS, should be removed soon
+     *
+     * Method invokes {@link #breakPoint()} method if caller of {@link #materialize(Source)} is not listed
+     * as known or valid use case. This is used to find all materialization sources of lazy deserialized
+     * containers (metadata)
+     */
+    @Deprecated
+    private void trackCallers() {
+        if (!TRACK_CALLERS) {
+            return;
+        }
+        if (callerIsNoneOf(
+                // "CryptoUtil", // Visitor, checking if protected values are encrypted
+                //"ContainerValueIdGenerator", // Visitor, checking for container value IDs
+                "AssignmentProcessor", // Assignment metadata
+                "ItemDeltaImpl", // Application of the delta.
+                "ShadowUpdater", // Shadow metadata
+                "AbstractMappingImpl", // Mapping provenance comparison
+                "OperationalDataManager", // Object metadata
+                "PrismObjectAsserter" // testing, we should not care about materialization
+        )) {
+            breakPoint();
+        }
+    }
+
+    private void breakPoint() {
+        parent.getPath();
+    }
+
+
+
+    private static boolean callerIsNoneOf(String... callers) {
+        for (var elem : new Throwable().fillInStackTrace().getStackTrace()) {
+            for (var caller : callers) {
+                if (elem.toString().contains(caller)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
