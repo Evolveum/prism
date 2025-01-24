e29c1bc3b7b8d5cada270c58acd91c8ef88d6134
Andrej Zan <andrej.zan@evolveum.com>
2025-01-24 12:56:17 +0100

    Add test for PropertyValue conversions improvement
    **What**
    
    Add test which may serve as acceptance criteria for new improvement
    MID-10372
    
    **Relates to:** MID-10372

---
+++ infra/prism-impl/src/test/java/com/evolveum/midpoint/prism/TestPrismPropertyValueImpl.java
@@ -0,0 +1,39 @@
+package com.evolveum.midpoint.prism;
+
+import java.io.IOException;
+import javax.xml.namespace.QName;
+
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.xml.sax.SAXException;
+
+import com.evolveum.midpoint.prism.impl.PrismContextImpl;
+import com.evolveum.midpoint.prism.impl.PrismPropertyDefinitionImpl;
+import com.evolveum.midpoint.prism.impl.PrismPropertyValueImpl;
+import com.evolveum.midpoint.util.DOMUtil;
+import com.evolveum.midpoint.util.exception.SchemaException;
+
+public class TestPrismPropertyValueImpl extends AbstractPrismTest {
+    @DataProvider
+    private Object[][] conversionParameters() {
+        return new Object[][] {
+                {DOMUtil.XSD_DOUBLE, Double.class},
+                {DOMUtil.XSD_FLOAT, Float.class}
+        };
+    }
+
+    @Test(enabled = false, description = "Not implemented. Implementation task - MID-10372",
+            dataProvider = "conversionParameters")
+    void valueIsInteger_applyDefinitionWithDouble_conversionShouldPass(QName definitionType, Class<?> convertedType)
+            throws SchemaException, IOException, SAXException {
+        final PrismPropertyValueImpl<Integer> value = new PrismPropertyValueImpl<>(10);
+
+        final PrismContextImpl prismContext = PrismInternalTestUtil.constructInitializedPrismContext();
+        final PrismPropertyDefinitionImpl<Object> definitionWithDouble = prismContext.definitionFactory()
+                .newPropertyDefinition(PrismInternalTestUtil.EXTENSION_NUM_ELEMENT, definitionType);
+
+        final Object realValue = value.applyDefinition(definitionWithDouble).getRealValue();
+        Assertions.assertThat(realValue).isInstanceOf(convertedType);
+    }
+}
