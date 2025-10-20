f9659ff743c84723a04898ae43bbeaedcc58b222
Viliam Repan <vilo.repan@evolveum.com>
2025-10-20 18:23:55 +0200

    MID-10865 found and fixed some license change problems

--- infra/prism-api/src/main/java/com/evolveum/midpoint/prism/PrismStaticConfiguration.java
+++ infra/prism-api/src/main/java/com/evolveum/midpoint/prism/PrismStaticConfiguration.java
@@ -6,12 +6,6 @@
 
 package com.evolveum.midpoint.prism;
 
-/*
- * Copyright (c) 2021 Evolveum and contributors
- *
- * This work is dual-licensed under the Apache License 2.0
- * and European Union Public License. See LICENSE file for details.
- */
 public class PrismStaticConfiguration {
 
     private static boolean serializationProxiesEnabled = false;
