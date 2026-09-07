40cfb3e9bc2290de760657b41c070b5ed709adf8
Viliam Repan <vilo.repan@evolveum.com>
2026-09-07 20:22:56 +0200

    Reverted back original behavior.
    Hint stays unencrypted unless it comes in delta. Check encrypted
    now allows to skip certain paths during validation (currently "hint").
    
    Task: 12111

--- infra/prism-api/src/main/java/com/evolveum/midpoint/prism/crypto/ProtectedData.java
+++ infra/prism-api/src/main/java/com/evolveum/midpoint/prism/crypto/ProtectedData.java
@@ -20,6 +20,10 @@ public interface ProtectedData<T> {
 
     T getClearValue();
 
+    default boolean hasClearValue() {
+        return getClearValue() != null;
+    }
+
     void setClearValue(T data);
 
     void destroyCleartext();
